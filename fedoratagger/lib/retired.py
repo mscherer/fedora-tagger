0da3422e0e15a55c878e7436d261acce8e217d59
elormes <ebuertey5@gmail.com>
2018-04-25 16:29:21 +0000

    port to PDC

--- fedoratagger/lib/retired.py
+++ fedoratagger/lib/retired.py
@@ -3,19 +3,11 @@ Tagger need to remove the packages in retired status.
 Is that the script Should be run as:
 
 FEDORATAGGER_CONFIG = /etc/fedora-tagger/fedora-tagger.cfg python /path/to/fedoratagger/lib/retired.py
-
-or
-
-FEDORATAGGER_CONFIG = /etc/fedora-tagger/fedora-tagger.cfg fedoratagger-remove-pkgs -s Retired
 """
 import requests
-import json
 
-import argparse
 import model as m
 import fedoratagger as ft
-
-import sqlalchemy
 
 import logging
 
@@ -23,49 +15,65 @@ log = logging.getLogger("fedoratagger-remove")
 log.setLevel(logging.DEBUG)
 logging.basicConfig()
 
-PKGDB_URL = 'https://admin.fedoraproject.org/pkgdb/api/packages'
+PDC_URL = 'https://pdc.fedoraproject.org/rest_api/v1/component-branches'
 
-def get_packages(status):
-    log.info('Retrieve the list of %s packages.' % status)
-    total = 1
-    page = 1
+
+def get_retired_packages():
+
     packages = []
-    while page <= total:
-        args = {
-            'status': status,
-            'page': page,
-        }
-        r = requests.get(PKGDB_URL, params=args)
-        output = json.loads(r.text)
-        if hasattr(output, 'error'):
-            log.error(output['error'])
-            return packages
 
-        total = output['page_total']
-        for pkg in output['packages']:
-            packages.append(pkg['name'])
-        page += 1
+    args = {
+        'name': 'f28',
+        'active': False,
+    }
+
+    response = requests.get(PDC_URL, args)
+    if response.ok:
+        output = response.json()
+    else:
+        response.raise_for_status()
+    for pkg in output['results']:
+        pkg_name = pkg['global_component']
+        packages.append(pkg_name)
+    pdc_api_url = output['next']
+
+    while pdc_api_url:
+        response = requests.get(pdc_api_url)
+        if response.ok:
+            output = response.json()
+        else:
+            response.raise_for_status()
+        for pkg in output['results']:
+            pkg_name = pkg['global_component']
+            packages.append(pkg_name)
+
+        pdc_api_url = output['next']
 
     return packages
 
-def del_packages(status):
+
+def del_packages():
 
     log.info('Deleting packages.')
 
     s = 0
     e = 100
-    pkgs = get_packages(status)
+    pkgs = get_retired_packages()
     l = len(pkgs)
     while (pkgs[s:e]):
         log.info('searching packages [%i] to [%i] of [%i]' % (s, e, l))
-        package = ft.SESSION.query(m.Package).filter(m.Package.name.in_(pkgs[s:e]))
+        package = ft.SESSION.query(
+            m.Package).filter(
+            m.Package.name.in_(pkgs[s:e]))
         s = e
         e = e + 100
 
         for r in package:
             tag = ft.SESSION.query(m.Tag).filter(m.Tag.package_id == r.id)
             usage = ft.SESSION.query(m.Usage).filter(m.Usage.package_id == r.id)
-            rating = ft.SESSION.query(m.Rating).filter(m.Rating.package_id == r.id)
+            rating = ft.SESSION.query(
+                m.Rating).filter(
+                m.Rating.package_id == r.id)
             for t in tag:
                 vote = ft.SESSION.query(m.Vote).filter(m.Vote.tag_id == t.id)
                 vote.delete()
@@ -76,21 +84,11 @@ def del_packages(status):
 
         ft.SESSION.commit()
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-          '-s', '--status',
-          dest='status',
-          default='Retired',
-          help="Restrict to status. {Approved, Retired, Orphaned}"
-         )
-    return parser.parse_args()
 
 def main():
 
-    args = parse_args()
+    del_packages()
 
-    del_packages(args.status.title())
 
 if __name__ == '__main__':
     main()
